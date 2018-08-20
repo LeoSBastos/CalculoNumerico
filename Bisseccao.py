b84366e4995e0a34ceddccfef866493a7a0a7d07
Luiz Gustavo Benicio Neves <luizgustavobenicio@gmail.com>
2018-08-20 09:13:20 -0300

    Update Bisseccao.py

--- Bisseccao.py
+++ Bisseccao.py
@@ -1,27 +1,34 @@
-#           //////,           _____ _                             
-#         */@&/////*         | __  |_|___ ___ ___ ___ ___ ___ ___ 
+#           //////,           _____ _
+#         */@&/////*         | __  |_|___ ___ ___ ___ ___ ___ ___
 #         *//////***         | __ -| |_ -|_ -| -_|  _|  _| .'| . |
 #     //////////**** &&&&    |_____|_|___|___|___|___|___|__,|___|
-#    /////////****** &&&    
+#    /////////****** &&&
 #    //////......./&&&&&&&    Algoritmo de Bissecção
 #    *//// &&&&&&&&&&&&&&(    Desenvolvido Por Luiz Gustavo Benicio Neves e Leonardo Soares
 #     ///*/&&&&&&&&&&&&&%     Para a Discliplina de Calculo Numérico
-#         /&&&&&&&&&          
-#          &&&&&&@@&          
-#            #&&                           
-                                       
+#         /&&&&&&&&&
+#          &&&&&&@@&
+#            #&&
+
 
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 eps = np.finfo(float).eps
-
+vala = []
+valb = []
+xo = []
 
 def f(x):
     return np.exp(x) - x -2
 
 def printar(n,a,b,x):
-    print("a: ",a,"b: " ,b,"x :",x)
+    print("iteracao:",n,"a: ",a,"b: " ,b,"x :",x)
+    vala.append(a)
+    valb.append(b)
+    xo.append(x)
+
 
 def bisseccao(a,b,iteracoes):
     if(iteracoes > 0):
@@ -45,9 +52,11 @@ def bisseccao(a,b,iteracoes):
                 else:
                     printar(iteracoes,a,b,x)
                     bisseccao(a,x,iteracoes-1)
+            else:
+                print("meu amigo, queria dizer não, mas vai ter que usar algo que ainda não aprendi")
     else:
         return None
-                    
+
 
 
 #Exemplo da Tabela 3.1 realizado do livro da UFRGS
@@ -55,6 +64,6 @@ a = float(-2)
 b = float(0)
 bisseccao(a,b,5)
 
-
-
-
+plt.plot(vala,valb,color="#0000ff")
+plt.plot(xo, color="#FF0000")
+plt.show()
