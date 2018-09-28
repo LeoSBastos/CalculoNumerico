4a602a8f22bf1d4aa8da1d7cccc9d0509ad7c7be
Leo Bastos <leosbastos7@gmail.com>
2018-09-28 22:58:06 +0000

    GRÁFICOS!!!!!!!

--- Trab1/Ex2.py
+++ Trab1/Ex2.py
@@ -1,29 +1,49 @@
 from Metodos import pontofixo
 from prettytable import PrettyTable
-vals = []
+import random
+import matplotlib.pyplot as plt
+
+valerror = []
 i = 20
 
 val = 2
-
-# def f(x):
-#     return ((x-1)*((math.exp(x-2))**2))-1
 
 def f(x):
     return x**2-1
 
 def g1(x):
-    return  x - f(x)
+    return  (x - (f(x)/10))
 
 def g2(x):
     return x-(f(x)/2)
 
 def g3(x):
-    return x + ((f(x)**1/2)*2.321*(11+24+101+69))
+    return x - (f(x)/1000)
 
 gs = [g1,g2,g3]
+
 erro=1e-6
 max_it=1000000000000
 tabela = PrettyTable(["Dado Inicial","xbarra","fi(x)","Erro","Número de Iterações"])
+
+def pontofixo(f,val,eps,max_i):
+    i=0
+    x = val
+    old = None
+    valores = []
+    while(i < max_i and old != x):
+        old = x
+        x=float(x - f(x))
+        i+=1
+        valerror.append(abs(old-x))
+        if(abs(x)>1e+20):
+            return None
+        if(abs(old-x)<eps):
+            x = random.uniform(old,x)
+    valores.append(old)
+    valores.append(x)
+    valores.append(i)
+    return valores
 
 for i in range(3):
     pf = pontofixo(gs[i],val,erro,max_it)
@@ -33,3 +53,6 @@ for i in range(3):
         tabela.add_row([val,pf[1],i+1,(abs(pf[0]-pf[1])),pf[2]])
 
 print(tabela)
+
+plt.plot(valerror, color="#FF0080")
+plt.show()
