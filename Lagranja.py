4aaace8c6d1f36f69a76563cc77c2c1a52d7668f
LeoSBastos <leosbastos7@gmail.com>
2018-10-09 18:19:37 -0300

    Agora imprime uhul

--- Lagranja.py
+++ Lagranja.py
@@ -1,20 +1,22 @@
-import numpy
-import sympy
+import numpy as np
+from sympy import *
 
 x=Symbol('x')
 
 def Lagranja(x,vet):
-    formula=None
-    forms=[]
+    formao=[]
     for i in range(len(vet)):
         forms=[]
         for j in range(len(vet)):
             if j!=i:
                 forms.append((x-vet[j][0])/(vet[i][0]-vet[j][0]))
-                k+=1
-        formula+=(vet[i][1]*forms[0]*form[1])
+        formao.append(vet[i][1]*forms[0]*forms[1])
+    formula = 0
+    for i in range(len(formao)):
+        formula += formao[i]
     return formula
 
-vet=[[0,1],[],[]]
+
+vet=[[-2,2],[0,-2],[4,1]]
 
 print(Lagranja(x,vet))
